b418e049abe1942c5ecf86563d5c17325ce90d9e
anwaarjunaid <junaidhmarcomians@gmail.com>
2023-12-26 15:47:10 +0530

    meny fixed

--- src/Containers/HomePage/MenuContaner/MenuContaner.tsx
+++ src/Containers/HomePage/MenuContaner/MenuContaner.tsx
@@ -1,11 +1,40 @@
+"use client";
 import Menu from "@/Components/Menu/Menu";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { menuData } from "../../../data/menuData";
 
 function MenuContaner() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (menuRef.current) {
+        const menuTop = menuRef.current.getBoundingClientRect().top;
+        console.log("ScrollTop:", window.scrollY);
+        console.log("MenuTop:", menuTop);
+
+        setIsScrolled(menuTop < 0);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="bg-primary p-3">
-      <div className=" max-w-[600px] mx-auto rounded-[25px]  p-4 border-solid bg-[#464344]">
+    <div className={`bg-primary p-3 `}>
+      <div
+        ref={menuRef}
+        className={`max-w-[600px] mx-auto rounded-[25px] p-4 border-solid bg-[#464344] ${
+          isScrolled
+            ? "fixed top-3 w-full z-50 left-1/2 transform -translate-x-1/2 will-change"
+            : ""
+        }`}
+      >
         <div className="flex gap-3">
           {menuData.map((item, index) => (
             <Menu key={index} text={item.label} />
