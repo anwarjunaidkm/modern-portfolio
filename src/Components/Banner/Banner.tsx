ac2b4d9beee8d7b429ea33a2bd17091d67d9fbbf
anwaarjunaid <junaidhmarcomians@gmail.com>
2023-12-26 15:00:02 +0530

    motion added

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,4 +1,7 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+
 interface bannerProps {
   text: String;
 }
@@ -6,10 +9,15 @@ interface bannerProps {
 function Banner({ text }: bannerProps) {
   return (
     <div className="text-white ">
-      <h1 className="text-9xl text-center leading-normal font-bold ">
+      <motion.h1
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2 }}
+        className="text-9xl text-center leading-normal font-bold "
+      >
         {text} <br />
         the <span className="px-1 text-violet-500">Gemini</span> era
-      </h1>
+      </motion.h1>
     </div>
   );
 }
