c1ef60f1eb66238f78f9b2ede172b084c8a71671
anwaarjunaid <junaidhmarcomians@gmail.com>
2023-12-26 12:46:14 +0530

    header ui fix

--- src/Containers/HomePage/HeaderContainer/HeaderContainer.tsx
+++ src/Containers/HomePage/HeaderContainer/HeaderContainer.tsx
@@ -6,11 +6,11 @@ import { FiSearch } from "react-icons/fi";
 
 function HeaderContainer() {
   return (
-    <Container>
-      <nav className=" flex justify-between bg-primary text-secondary h-16 items-center">
+    <nav className="  flex justify-between bg-primary text-secondary h-16 items-center">
+      <div className=" flex mx-auto justify-between w-full max-w-[1200px] ">
         <div className=" flex gap-5 ">
           <h1>logo</h1>
-          
+
           <div className=" flex gap-4 text-2xl font-bold ">
             {navData.map((item, index) => (
               <Navbar key={index} label={item.label} url={item.url} />
@@ -18,11 +18,10 @@ function HeaderContainer() {
           </div>
         </div>
         <div>
-        <FiSearch className="w-6 h-6"/>
-
+          <FiSearch className="w-6 h-6" />
         </div>
-      </nav>
-    </Container>
+      </div>
+    </nav>
   );
 }
 
