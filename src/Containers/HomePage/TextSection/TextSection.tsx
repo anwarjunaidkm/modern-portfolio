6a17931867e4da691701f6ad80985f0898436897
anwaarjunaid <junaidhmarcomians@gmail.com>
2023-12-26 14:42:33 +0530

    text section aded

---
+++ src/Containers/HomePage/TextSection/TextSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TextSection() {
+  return (
+    <div>
+      <div className="h-screen bg-primary flex justify-center flex-col items-center text-secondary w-full">
+        <p className=" w-[60%] leading-relaxed font-extralight text-6xl">
+          <h1 className="text-8xl font-semibold">Our Story</h1>
+          In a world brimming with possibilities, we sprouted, seizing our
+          destiny to create an extraordinary narrative - a tale that
+          encapsulates laughter, tears, challenges, and triumphs...!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TextSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import BannerContainer from "@/Containers/HomePage/BannerContainer/BannerContainer";
 import HeaderContainer from "@/Containers/HomePage/HeaderContainer/HeaderContainer";
 import MenuContaner from "@/Containers/HomePage/MenuContaner/MenuContaner";
+import TextSection from "@/Containers/HomePage/TextSection/TextSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeaderContainer />
       <BannerContainer />
       <MenuContaner />
+      <TextSection />
     </>
   );
 }
